6e2f78b1ca7bc3f91a60674e315a472d502292e8
name <adityagaikwad009@gmail.com>
2017-09-14 00:53:20 +0530

    Login, register, index done

--- mysite/trackAmazon/urls.py
+++ mysite/trackAmazon/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name="index")
+    url(r'^$', index, name="index"),
+    url(r'^login$', login, name="login"),
+    url(r'^register$', register, name="register")
 ]
--- mysite/trackAmazon/views.py
+++ mysite/trackAmazon/views.py
@@ -4,3 +4,9 @@ def index(request):
 
     di = {"ki": "hi", 2 : {"ko": 2, "q": 3}}
     return render(request, "index.html", di)
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
